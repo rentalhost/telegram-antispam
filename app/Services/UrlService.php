55de65d354c3a8e6a396203cfb34e2d695502aa5
David Rodrigues <david.rodrigues@rentalhost.net>
2022-10-05 13:18:49 -0300

    [Fixed] UrlService: accept non-www URLs;

--- app/Services/UrlService.php
+++ app/Services/UrlService.php
@@ -9,7 +9,7 @@ class UrlService
     /** @noinspection PhpArrayShapeAttributeCanBeAddedInspection */
     private static function normalizeUrl(string $url): string
     {
-        return parse_url('https://' . strtolower(preg_replace('#^(https?://)?(www|m).#', '', $url)), PHP_URL_HOST);
+        return parse_url('https://' . strtolower(preg_replace('#^(https?://)?((www|m)[.])?#', '', $url)), PHP_URL_HOST);
     }
 
     public static function isDomainAllowed(string $url): bool
--- config/antispam.php
+++ config/antispam.php
@@ -5,11 +5,14 @@ declare(strict_types = 1);
 return [
     'allowedDomains' => [
         'twitch.tv',
-        'discord.com',
         'thetraveler.group',
         'raid.report',
         'dungeon.report',
         'grandmaster.report',
+
+        // Discord related.
+        'discord.com',
+        'discord.gg',
 
         // Youtube related.
         'youtube.com',
